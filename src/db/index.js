bc5d1cb568808c757ee90e81e30e5f8f103bffea
Fabian <fabianterh@gmail.com>
2019-02-13 16:15:46 +0800

    Run eslint

--- src/db/index.js
+++ src/db/index.js
@@ -2,19 +2,19 @@ const sqlite3 = require("sqlite3").verbose();
 const config = require("./../../config");
 
 // Set up
-const db = new sqlite3.Database(config.database.name, (err) => {
+const db = new sqlite3.Database(config.database.name, err => {
   if (err) console.error(err);
   console.log(`Connected to ${config.database.name}`);
 });
 
 function writeToTeachersDatabase(teacher, student) {
-  let sql = `INSERT INTO teachers (teacher_email, student_email) \
+  const sql = `INSERT INTO teachers (teacher_email, student_email) \
     VALUES ("${teacher}", "${student}")`;
-    db.run(sql);
+  db.run(sql);
 }
 
 function readFromTeachersDatabase(teacher) {
-  let sql = `SELECT * FROM teachers WHERE teacher_email = "${teacher}"`;
+  const sql = `SELECT * FROM teachers WHERE teacher_email = "${teacher}"`;
   return new Promise((resolve, reject) => {
     db.all(sql, (err, rows) => {
       if (err) reject(err);
@@ -24,18 +24,18 @@ function readFromTeachersDatabase(teacher) {
 }
 
 function writeToStudentsDatabase(student, suspendedStatus) {
-  let suspendedStatusConverted = suspendedStatus ? "1" : "0";
-  let sql = `INSERT INTO students (student_email, suspended_status)
+  const suspendedStatusConverted = suspendedStatus ? "1" : "0";
+  const sql = `INSERT INTO students (student_email, suspended_status)
     VALUES ("${student}", "${suspendedStatusConverted}")`;
-    db.run(sql);
+  db.run(sql);
 }
 
 function isStudentSuspended(student) {
-  let sql = `SELECT * FROM students WHERE student_email = "${student}"`;
+  const sql = `SELECT * FROM students WHERE student_email = "${student}"`;
   return new Promise((resolve, reject) => {
     db.get(sql, (err, result) => {
       if (err) reject(err);
-      if (result.suspended_status == 0) {
+      if (result.suspended_status === 0) {
         resolve(false);
       } else {
         resolve(true);
