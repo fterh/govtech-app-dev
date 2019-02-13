bd3f798e2d8b0a9abd6174c21ef843d3d9a8b1cb
Fabian <fabianterh@gmail.com>
2019-02-13 18:28:25 +0800

    Implement POST /api/suspend route

--- src/server/routes/suspend/post.js
+++ src/server/routes/suspend/post.js
@@ -1,5 +1,19 @@
+const extractFromObject = require("./../../../utils/extractFromObject");
+const { markStudentSuspension } = require("./../../../db");
+
 function post(req, res) {
-  res.status(204).send("It works!");
+  const payload = req.body;
+  const studentToSuspend = extractFromObject("student", payload);
+
+  if (typeof studentToSuspend !== "string") {
+    res.status(400).send({
+      message: "`student` must be a string"
+    });
+  }
+
+  markStudentSuspension(studentToSuspend, true);
+
+  res.status(204).send();
 }
 
 module.exports = post;
