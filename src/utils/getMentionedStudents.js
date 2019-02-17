e7d3426a7bed7cd9582b062f25d642c9c688317a
Fabian <fabianterh@gmail.com>
2019-02-17 14:20:12 +0800

    Fix lint errors

--- src/utils/getMentionedStudents.js
+++ src/utils/getMentionedStudents.js
@@ -1,16 +1,16 @@
 /**
  * Extracts and returns an array of mentioned students from a notification.
- * @param {string} notification 
+ * @param {string} notification
  */
 function getMentionedStudents(notification) {
   const regex = /@.+?( |$)/g;
   const matches = notification.match(regex) || []; // Empty array if no matches
-  let res = [];
-  
+  const res = [];
+
   matches.forEach(match => {
-    match = match.slice(1); // Remove "@" symbol
-    match = match.trim(); // Remove whitespace
-    res.push(match)
+    const matchSliced = match.slice(1); // Remove "@" symbol
+    const matchTrimmed = matchSliced.trim(); // Remove whitespace
+    res.push(matchTrimmed);
   });
 
   return res;
