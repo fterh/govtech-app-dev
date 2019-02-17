2bfda950e55a8a4f78a908c9f3910e90e876fb5e
Fabian <fabianterh@gmail.com>
2019-02-17 14:30:15 +0800

    Implement POST /api/retrievefornotifications route

--- src/server/routes/retrievefornotifications/post.js
+++ src/server/routes/retrievefornotifications/post.js
@@ -1,5 +1,82 @@
+const extractFromObject = require("./../../../utils/extractFromObject");
+const getMentionedStudents = require("./../../../utils/getMentionedStudents");
+const {
+  readFromTeachersDatabase,
+  isStudentSuspended
+} = require("./../../../db");
+
 function post(req, res) {
-  res.status(200).send("It works!");
+  const payload = req.body;
+  let teacher;
+  let notification;
+  try {
+    teacher = extractFromObject("teacher", payload);
+  } catch {
+    res.status(500).send({
+      message: 'You must provide a "teacher" property'
+    });
+    return;
+  }
+  try {
+    notification = extractFromObject("notification", payload);
+  } catch {
+    res.status(500).send({
+      message: 'You must provide a "notification" property'
+    });
+    return;
+  }
+
+  const mentionedStudents = getMentionedStudents(notification);
+
+  readFromTeachersDatabase(teacher)
+    .then(students => {
+      const registeredStudents = students;
+
+      // Remove duplicates
+      const allStudentsSet = new Set([
+        ...mentionedStudents,
+        ...registeredStudents
+      ]);
+      const allStudents = Array.from(allStudentsSet);
+
+      // Remove suspended students
+      const studentStatuses = {}; // Dictionary mapping student to suspension status
+      const promises = [];
+      allStudents.forEach(student => {
+        promises.push(
+          isStudentSuspended(student).then(suspended => {
+            studentStatuses[student] = suspended;
+          })
+        );
+      });
+
+      Promise.all(promises)
+        .then(() => {
+          const recipients = [];
+          Object.entries(studentStatuses).forEach(studentStatus => {
+            if (!studentStatus[1]) {
+              // Not suspended
+              recipients.push(studentStatus[0]);
+            }
+          });
+
+          res.status(200).send({
+            recipients
+          });
+        })
+        .catch(e => {
+          console.error(e);
+          res.status(500).send({
+            message: "Something went wrong internally"
+          });
+        });
+    })
+    .catch(e => {
+      console.error(e);
+      res.status(500).send({
+        message: "Something went wrong internally"
+      });
+    });
 }
 
 module.exports = post;
